cc6b75e105c79ddecc5fd5b215a32d760230cfaf
15-sick <130491266+Blacera@users.noreply.github.com>
2023-05-27 02:22:54 +0700

    Add files via upload

---
+++ pheptoanshoc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    long long int a,b;
+    cin >> a;
+    cin >> b;
+    cout << a+b << "\n" ;
+    cout << a-b << "\n";
+    cout << a*b << "\n";
+    cout << a/b;
+    return 0;
+}
