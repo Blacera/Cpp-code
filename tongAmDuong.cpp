14fd69fa8b2cfda49ac3b50a7a5fd8c59f765384
Woncyk <130491266+Blacera@users.noreply.github.com>
2023-07-10 14:51:35 +0700

    Add files via upload

---
+++ tongAmDuong.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a + b < 0)
+    {
+        cout << "Tong am";
+    }
+    if (a + b >= 0)
+    {
+        cout << "Tong duong";
+    }
+    return 0;
+}
